5ef6335b31056910bc21afd830f6d8937207039e
Uday Ladhi <105300620+Udayl56@users.noreply.github.com>
2024-04-27 13:21:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://js-coding-stack-vite.vercel.app'
 }));
 
 app.use(express.json());
